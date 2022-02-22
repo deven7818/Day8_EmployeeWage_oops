560179a78e87ee825070286fd61da9a2e36963f2
deven7818 <deven7818@gmail.com>
2022-02-22 22:37:22 +0530

    Empolyee wage with Opps concepts

---
+++ EmpWageOpps/src/main/java/com/empwageopps/App.java
@@ -0,0 +1,9 @@
+package com.empwageopps;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Employee Wage Computation..!" );
+    }
+}
